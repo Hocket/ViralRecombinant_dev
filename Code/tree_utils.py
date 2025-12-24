684b82d755fe69a92276a07043c9ebdef760c151
atp7064 <atp7064@g.rit.edu>
2025-12-23 22:19:27 -0500

    Add color_tree function to annotate branches with colors based on unique recombinant status

--- Code/tree_utils.py
+++ Code/tree_utils.py
@@ -44,3 +44,40 @@ def extract_epi_isl(name):
     """Extracts the EPI_ISL identifier from a sample name string."""
     match = re.search(r"EPI_ISL_\d+", name)
     return match.group(0) if match else None
+
+
+def color_tree(data, tree_file, output_file):
+    """ writes comments on branches to state color
+
+    Args:
+        data (dataFrame): pandas dataframe representing a tree
+        tree_file (string): A newick style tree file to be colored
+        output_file (string): The output file for the colored newick tree
+    """
+
+    # pass through and add color to tree
+    tree = Phylo.read(tree_file, "newick")
+    
+    df = data.set_index("epi_isl")
+    for clade in tree.get_terminals():
+        sample_name = clade.name
+        epi_isl = extract_epi_isl(sample_name)
+        if epi_isl is None:
+            continue
+        unique_recombinant = (
+            df.at[epi_isl, "UniqueRecombinant"]
+            if epi_isl in df.index
+            else None
+        )
+        # COLORS USE HEX CODE
+        if unique_recombinant.lower() == "yes":
+            clade.comment = "&color=#FF0000"
+            continue
+        # TO IMPLEMENT WHEN COLORS ARE KNOWN
+        # num_pairs = (
+        #     df.at[epi_isl, "Num Pairs"]
+        #     if epi_isl in df.index
+        #     else None
+        # )
+    Phylo.write(tree, output_file, "newick")
+    print("Color tree saved to", output_file)
